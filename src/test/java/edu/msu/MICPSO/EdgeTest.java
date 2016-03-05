e998c2b6b95743b2a0e9a073c2c3ccc880583709
Rollie Goodman <rolliegoodman@gmail.com>
2016-03-05 12:54:16 -0700

    test folder is now tracked

---
+++ src/test/java/edu/msu/MICPSO/EdgeTest.java
@@ -0,0 +1,14 @@
+package edu.msu.MICPSO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EdgeTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, 3-2 > 0);
+	}
+
+}
