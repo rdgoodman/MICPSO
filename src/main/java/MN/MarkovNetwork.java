5271397b047912e716ed95b73a9d359972c7ab75
Rollie Goodman <rolliegoodman@gmail.com>
2016-03-06 16:06:59 -0700

    added some commented todos in the MN class, to do with network structure

--- src/main/java/MN/MarkovNetwork.java
+++ src/main/java/MN/MarkovNetwork.java
@@ -1,5 +1,21 @@
 package MN;
 
 public class MarkovNetwork {
+	
+	
+	
+	
+	
+	private void createNetworkStructure(){
+		// TODO: finish this
+		
+		// step 1: create nodes
+		
+		// step 2: create edges 
+		
+		// step 3: ...with any luck, we can have the Node class keep track of its 
+		// own Markov blanket
+		
+	}
 
 }
