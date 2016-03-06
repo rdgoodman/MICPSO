bccc50645249063d325ff3c2fa96455375137265
Rollie Goodman <rolliegoodman@gmail.com>
2016-03-06 16:31:20 -0700

    now we have some commented instructions in the Gibbs sampling stub method

--- src/main/java/MN/MarkovNetwork.java
+++ src/main/java/MN/MarkovNetwork.java
@@ -1,8 +1,11 @@
 package MN;
 
+import java.util.ArrayList;
+
 public class MarkovNetwork {
 	
-	
+	private ArrayList<Node> nodes;
+	private ArrayList<Edge> edges;
 	
 	
 	
@@ -24,6 +27,29 @@ public class MarkovNetwork {
 	 */
 	private Sample Sample(){
 		// TODO: stub
+		
+		// 1) generate an initial sample (probably randomly from vals(Vars)
+		
+		// 2) For some number of iterations (I think)...
+		
+		// 3) For each non-evidence variable (so, all of them) [order doesn't matter]...
+		
+		// 4) Pick a variable, X
+		
+		// 5) Calculate P(X|MB(X)) using current values for MB(X)
+		
+		// 6) Resample value of X using the above distribution
+		
+		// 6a) For each value in vals(X), calculate basically the clique potential
+		// (in red on notes from 3/6)
+		
+		// 6b) Normalize results
+		
+		// 6c) Sample
+		
+		// end for
+		
+		
 		return null;
 	}
 }
