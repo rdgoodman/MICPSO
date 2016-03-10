5e4cb366820879a1718441409974c09f8b7c7448
Rollie Goodman <rolliegoodman@gmail.com>
2016-03-10 11:58:03 -0700

    added some initial sampling for first step of Gibbs sampling, currently untested

--- src/main/java/MN/MarkovNetwork.java
+++ src/main/java/MN/MarkovNetwork.java
@@ -3,58 +3,94 @@ package MN;
 import java.util.ArrayList;
 
 public class MarkovNetwork {
-	
+
 	private ArrayList<Node> nodes;
 	private ArrayList<Edge> edges;
-	
-	public MarkovNetwork(){
+
+	public MarkovNetwork() {
 		// TODO: read in from file
+		nodes = new ArrayList<Node>();
+		edges = new ArrayList<Edge>();
 	}
-	
-	
+
 	/**
 	 * Use this to create the network structure (known) for the network
 	 */
-	private void createNetworkStructure(){
+	private void createNetworkStructure() {
 		// TODO: finish this
-		
+
 		// step 1: create nodes
-		
-		// step 2: create edges 
-		
-		// step 3: ...with any luck, we can have the Node class keep track of its 
-		// own Markov blanket		
+
+		// step 2: create edges
+
+		// step 3: ...with any luck, we can have the Node class keep track of
+		// its
+		// own Markov blanket
 	}
 
 	/**
 	 * Returns the result of Gibbs sampling this distribution
+	 * 
 	 * @return
 	 */
-	private Sample Sample(){
-		// TODO: stub
-		
+	private Sample Sample() {
+
+		Sample sample = new Sample(this);
+
 		// 1) generate an initial sample (probably randomly from vals(Vars)
-		
-		// 2) For some number of iterations (I think)...
-		
-		// 3) For each non-evidence variable (so, all of them) [order doesn't matter]...
-		
-		// 4) Pick a variable, X
-		
-		// 5) Calculate P(X|MB(X)) using current values for MB(X)
-		
-		// 6) Resample value of X using the above distribution
-		
-		// 6a) For each value in vals(X), calculate basically the clique potential
+		for (Node n : nodes) {
+			// TODO: chosen uniformly - does that work?
+			double[] vals = n.getVals();
+			double r = Math.random();
+			double interval = 1 / vals.length;
+
+			// TODO: holy guacamole, test this
+			int counter = 0;
+			for (double i = 0; i <= 1; i += interval) {
+				if (r < i) {
+					sample.setInitialValue(n, vals[counter]);
+					break;
+				}
+				counter++;
+			}
+		}
+
+		// 2) For each non-evidence variable (so, all of them) [order doesn't
+		// matter]...
+
+		// 3) Pick a variable, X
+
+		// 4) Calculate P(X|MB(X)) using current values for MB(X)
+
+		// 5) Resample value of X using the above distribution
+
+		// 5a) For each value in vals(X), calculate basically the clique
+		// potential
 		// (in red on notes from 3/6)
-		
-		// 6b) Normalize results
-		
-		// 6c) Sample
-		
+
+		// 5b) Normalize results
+
+		// 5c) Sample
+
 		// end for
-		
-		
+
+		// TODO: stub
 		return null;
+	}
+
+	public ArrayList<Node> getNodes() {
+		return nodes;
+	}
+
+	public void setNodes(ArrayList<Node> nodes) {
+		this.nodes = nodes;
+	}
+
+	public ArrayList<Edge> getEdges() {
+		return edges;
+	}
+
+	public void setEdges(ArrayList<Edge> edges) {
+		this.edges = edges;
 	}
 }
--- src/main/java/MN/Sample.java
+++ src/main/java/MN/Sample.java
@@ -1,10 +1,28 @@
 package MN;
 
+import java.util.Hashtable;
+
 public class Sample {
 
 	
 	// TODO: will contain the results of a Gibbs sampling run
 	// This will (directly or indirectly) need to be able to be fed into our fitness function
+	// will we be using an index or ID or something for nodes? I don't freaking know
+	
+	private Hashtable<Node, Double> values;
+	
+	public Sample(MarkovNetwork mn){
+		values = new Hashtable<Node, Double>();
+	}
+	
+	/**
+	 * Sets the initial value
+	 * @param n Node which has been sampled
+	 * @param value Value sampled for the node's variable
+	 */
+	protected void setInitialValue(Node n, Double value){
+		values.put(n, value);
+	}
 	
 	
 }
