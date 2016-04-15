4bf695cad74f19aea1b63c5f5f10a2b77015435f
Rollie Goodman <rolliegoodman@gmail.com>
2016-04-15 13:50:03 -0600

    MNPSO is performing well for small test problems

---
+++ src/test/java/edu/msu/MICPSO/MNPSOTest.java
@@ -0,0 +1,20 @@
+package edu.msu.MICPSO;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+import PSO.ICPSO;
+
+public class MNPSOTest {
+
+	@Test
+	public void test() throws FileNotFoundException {
+		String filename = ("src/main/resources/graphColorTestFile.txt");
+		ICPSO p = new ICPSO(filename, true, 2, 3, 0.9, 0.4, 0.6, 0.6);
+		p.run();
+	}
+
+}
