b09ca6b410e6773c2e30a52211d4a003642c76cb
Rollie Goodman <rolliegoodman@gmail.com>
2016-05-14 15:49:51 -0600

    reversed changes to constraint checking since this should only need to apply to certain methods

--- src/main/java/applicationProblems/GraphColoringConstraint.java
+++ src/main/java/applicationProblems/GraphColoringConstraint.java
@@ -11,20 +11,8 @@ public class GraphColoringConstraint implements Constraint {
 		// TODO
 	}
 
-//	@Override
-//	public boolean satisfiesConstraint(Sample s, ArrayList<Edge> edgesArray) {
-//		for (Edge e: edgesArray){
-//			// invalid if any neighboring nodes have the same color
-//			if (s.getTable().get(e.getEndpoints().getFirst()) == s.getTable().get(e.getEndpoints().getLast())){
-//				System.out.println(e.getEndpoints().getFirst().getName() + " has the same color as " + e.getEndpoints().getLast().getName());
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
-	
 	@Override
-	public boolean satisfiesConstraint(Sample s) {
+	public boolean satisfiesConstraint(Sample s, ArrayList<Edge> edgesArray) {
 		for (Edge e: edgesArray){
 			// invalid if any neighboring nodes have the same color
 			if (s.getTable().get(e.getEndpoints().getFirst()) == s.getTable().get(e.getEndpoints().getLast())){
@@ -34,5 +22,7 @@ public class GraphColoringConstraint implements Constraint {
 		}
 		return true;
 	}
+	
+
 
 }
